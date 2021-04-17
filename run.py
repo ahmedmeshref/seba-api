79c24cfe08926ba696a5767b0354bf0c60e649c3
ahmedmeshref <a.meshref@alustudent.com>
2021-04-17 02:57:02 +0200

    Update teh app run file

--- run.py
+++ run.py
@@ -1,19 +1,5 @@
-from app import app
-import logging
-from logging import Formatter, FileHandler
+from app import create_app
 
-
-if not app.debug:
-    file_handler = FileHandler('error.log')
-    file_handler.setFormatter(
-        Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
-    )
-    app.logger.setLevel(logging.INFO)
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-    app.logger.info('errors')
-
-
-# Default port:
-if __name__ == '__main__':
+if __name__ == "__main__":
+    app = create_app()
     app.run(threaded=True, host='0.0.0.0', port=9000)
